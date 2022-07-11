759ef5ddd747e6cf89e16d1fcabc0fde3fee3051
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-07-11 19:34:07 +0400

    test

---
+++ bloc_package/lib/package/bloc_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BlocView extends StatefulWidget {
+  const BlocView({Key? key}) : super(key: key);
+
+  @override
+  State<BlocView> createState() => _BlocViewState();
+}
+
+class _BlocViewState extends State<BlocView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
